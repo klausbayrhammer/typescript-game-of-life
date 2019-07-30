7774094e7fa9bf0414c318972e6652f032d3a45c
Klaus Bayrhammer <klaus.bayrhammer@redbull.com>
2019-07-30 16:58:25 +0200

    initial commit

---
+++ test/index.spec.ts
@@ -0,0 +1,9 @@
+import {expect} from 'chai';
+import index from '../main/index';
+
+describe('test', function () {
+    it('should ', function () {
+        expect(index()).to.equal(2);
+    });
+
+});
